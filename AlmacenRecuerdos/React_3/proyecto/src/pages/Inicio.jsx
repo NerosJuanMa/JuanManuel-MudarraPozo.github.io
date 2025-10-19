cc51d2af9c408effd576814dfface76757158d93
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-19 13:33:27 +0200

    del_19_10_2025

--- AlmacenRecuerdos/React_3/proyecto/src/pages/Inicio.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Inicio() {
-  return (
-    <>   
-       <br /><br /><br /><br /><br /><br />  
-   <h1>Prueba inicio</h1>
-    </>
-    )
-}
--- AlmacenRecuerdos/src/Components/BotonAlerta.jsx
+++
@@ -1,8 +0,0 @@
-function BotonAlerta() {
-    return (
-        <button onClick={() => alert("¡Hola! Has hecho clic.")}>
-            Haz clic aquí
-        </button>
-    );
-}
-export default BotonAlerta;
